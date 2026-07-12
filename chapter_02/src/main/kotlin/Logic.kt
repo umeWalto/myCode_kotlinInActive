735be2f520e15304cb82d60172ebc37857ced699
Nikita <waltonsdom@gmail.com>
2026-07-12 21:22:02 +0500

    upd: Update Logic, new fun for screening

--- chapter_02/src/main/kotlin/Logic.kt
+++ chapter_02/src/main/kotlin/Logic.kt
@@ -1,2 +1,21 @@
 package org.example
 
+val billBoard = mutableMapOf<Char, Screening>()
+
+fun createSampleData(mapIndex: Char, hall: Hall, movieType: Movie) {
+    billBoard[mapIndex] = Screening(movie = movieType, hall = hall)
+}
+
+fun getScreening(input: Char) : Screening? = billBoard[input]
+
+fun allScreening() : String {
+    var poster = ""
+    for ((key, screening) in billBoard) {
+        poster += "$key. ${screening.movie} (зал ${screening.hall.hallId})\n"
+    }
+    return poster
+}
+
+fun bookSeat(row: Int, col: Int, mapIndex: Char) : Boolean {
+    return billBoard[mapIndex]?.hall?.bookSeat(row, col) ?: false
+}
